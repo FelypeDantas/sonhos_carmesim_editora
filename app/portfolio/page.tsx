946489e19553e71a08feefdf8fe2ca4f95238297
Felype <felyped03@gmail.com>
2026-06-24 15:23:27 -0300

    Update page.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Hero } from "@/components/PortfolioPage/Hero";
+import { PortfolioHero } from "@/components/PortfolioPage/Hero";
 import Link from "next/link";
 
 const sections = [
